0948a0abc1144e4ea3ed55fe467f0abd642631c6
bgros <bgros@DESKTOP-4340SIV>
2023-07-13 21:30:16 -0600

    Semana 8

--- src/main/java/com/tienda/domain/Producto.java
+++ src/main/java/com/tienda/domain/Producto.java
@@ -21,11 +21,24 @@ public class Producto implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_producto")
     private long idProducto;
+
+    
     private String descripcion;
+    private String detalle;
+    private double precio;
+    
+    private int existencias;
     private String rutaImagen;
     private boolean activo;
+    
+    
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria;
 
- 
+   
+  
+    
 
     public Producto() {
     }
