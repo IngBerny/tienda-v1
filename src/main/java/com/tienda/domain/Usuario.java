fcf887a18480c7514e6e4888cecd81c22cbad2ee
bgros <bgros@DESKTOP-4340SIV>
2023-08-03 16:27:48 -0600

    Semana 11 Correcciones Nuevas

--- src/main/java/com/tienda/domain/Usuario.java
+++ src/main/java/com/tienda/domain/Usuario.java
@@ -8,6 +8,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "usuario")
+
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -15,19 +16,19 @@ public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_usuario")
-    private long idUsuario;
+    private Long idUsuario;
+
     private String username;
     private String password;
-    private String nombre ;
-    private String apellidos ;
-    private String correo ;
-    private String telefono ;
-    private String rutaImagen ;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+    private String rutaImagen;
     private boolean activo;
 
     @OneToMany
     @JoinColumn(name = "id_usuario")
-    private List<Rol> roles;
-
+    List<Rol> roles;
 
 }
