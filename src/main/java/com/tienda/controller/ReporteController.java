b8df882bbb27134d85b861219a54163e8b8f1dda
bgros <bgros@DESKTOP-4340SIV>
2023-08-10 16:57:09 -0600

    Semana 12

---
+++ src/main/java/com/tienda/controller/ReporteController.java
@@ -0,0 +1,36 @@
+package com.tienda.controller;
+
+import com.tienda.domain.Usuario;
+import com.tienda.service.ReportService;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/reportes")
+
+public class ReporteController {
+    
+    @Autowired
+    ReportService reportService;
+    
+    @GetMapping("/principal")
+    public String principal(Model model) {
+        return "/reportes/principal";
+    }
+    
+    @GetMapping("/usuarios")
+    public ResponseEntity<Resource> usuarios(@RequestParam String tipo) throws IOException {
+        return reportService.generaReporte("usuarios",null,tipo);
+    }
+    @GetMapping("/ventas")
+    public ResponseEntity<Resource> ventas(@RequestParam String tipo) throws IOException {
+        return reportService.generaReporte("ventas",null,tipo);
+    }
+}
---
+++ src/main/java/com/tienda/service/ReportService.java
@@ -0,0 +1,19 @@
+
+package com.tienda.service;
+
+import java.io.IOException;
+import java.util.Map;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+
+public interface ReportService {
+    
+    public ResponseEntity<Resource>
+            generaReporte(
+                    String reporte, 
+                    Map<String, Object> parametros,
+                    String tipo
+            )throws IOException;
+            
+            
+}
